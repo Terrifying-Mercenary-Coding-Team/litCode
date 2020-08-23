eadf83cb9fae02d2911bd21bdcd780b237a29198
2015004248 <epikjjh@gmail.com>
2020-08-23 23:46:46 +0900

    08.23 Commit: JoHyungJung language: C++11 logic: two pointers
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

---
+++ others/linked_list_cycle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !(head->next))   return false;
+        ListNode *first=head;
+        ListNode *second=head->next;
+        while(first!=second){
+            if(!(second->next) || !(second->next->next))  return false;
+            first = first->next;
+            second = second->next->next;
+        }
+        return true;
+    }
+};
